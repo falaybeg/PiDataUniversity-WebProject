ce867a8f251c94299e5c8a64881ff71d97072859
falaybeg <furkanalaybeg@gmail.com>
2019-11-04 09:37:25 +0300

    Web template theme added

---
+++ PiDataApp/Areas/Fakulteler/Controllers/DepartmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NTierApp.Areas.Fakulteler.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Fakulteler/Department
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PiDataApp/Areas/Fakulteler/FakultelerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace NTierApp.Areas.Fakulteler
+{
+    public class FakultelerAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Fakulteler";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Fakulteler_default",
+                "Fakulteler/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
